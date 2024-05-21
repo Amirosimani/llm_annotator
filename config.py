1ee0b3bd21aad92a1e63ad0ffc1f6f4e6b531822
amir-cloudtop <amirimani@google.com>
2024-05-21 19:23:16 +0000

    moveed model name to config, added palm

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
-VALID_MODELS = ["gemini", "claude"]
+VALID_MODELS = ["gemini", "claude", "palm"]
+
+VERBOSE=False
 
 GEMINI_CONFIG = {
+    "model": "gemini-1.0-pro-002",
     "project_config": {
-        "qpm": 5,
+        "qpm": 300,
         "project": "amir-genai-bb", 
         "location": "us-central1"
     },
@@ -13,7 +16,22 @@ GEMINI_CONFIG = {
     }
 }
 
+PALM_CONFIG = {
+    "model": "text-bison-32k@002",
+    "project_config": {
+        "qpm": 300,
+        "project": "amir-genai-bb", 
+        "location": "us-central1"
+    },
+    "generation_config": {
+        "max_output_tokens": 1024,
+        "temperature": 0.1,
+        "top_p": 1,
+    }
+}
+
 CLAUDE_CONFIG = {
+    "model":"claude-3-haiku@20240307",
     "project_config": {
         "qpm": 60,  # Adjust as needed
         "project": "cloud-llm-preview1", 
@@ -23,4 +41,3 @@ CLAUDE_CONFIG = {
 }
 
 
-VERBOSE=False
